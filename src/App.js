5ea468e8a774ff9606375bcd15882a987ec6f7d9
Yander <yanderceara@gmail.com>
2023-05-11 23:47:25 -0300

    Criando um carrosel de imagens com react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import image1 from './images/image001.jpg'
+import image2 from './images/image002.jpg'
+import image3 from './images/image003.jpg'
+import image4 from './images/image004.jpg'
+import React, { useEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 import './App.css';
 
+const array_images = [image1, image2, image3, image4];
+
 function App() {
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        whileTap={{ cursor: 'grabbing' }}
+        className="carrousel"
+        ref={carousel}>
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          initial={{ x: 0 }}
+          className="inner">
+          {array_images?.map(image => (
+            <motion.div className="item">
+              <img src={image} alt="image" />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
